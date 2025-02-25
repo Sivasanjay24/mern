9ebd69fca60a4ddefea0b1827d58f4158cf111d9
Sivasanjay24 <sanjaysanjith2004@gmail.com>
2025-02-25 13:36:52 +0530

    an12

--- p-backend/index.js
+++ p-backend/index.js
@@ -1,16 +1,41 @@
-const express = require('express');
-
+const express = require("express");
+const Signup = require("./models/signupschema");
+const dotenv = require("dotenv");
+dotenv.config();
 const app = express();
-
-app.get('/1', (req, res) => 
-{
-    res.send('<br><h1 style = "text-align:center">This is the Backend Server running</h1>');
-})
-app.get('/', (req, res) => 
-{
-  res.sendFile("C:\\Users\\sanja\\Desktop\\mern\\Day-3\\bcss.html");
-})
-
+const mdb = require("mongoose");
+app.use(express.json())
 const port = 3000;
 
+mdb
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => {
+    console.log("Failed", err);
+  });
+app.post("/signup", (req, res) => {
+  try 
+  {
+    const { firstname, lastname, email, password, phonenumber } = req.body;
+    const signups = new Signup({
+      firstname: firstname,
+      lastname: lastname,
+      email: email,
+      password: password,
+      phonenumber: phonenumber,
+    });
+    signups.save();
+    console.log("Signup successful");
+    console.log(req.body)
+    res.status(201).json({ message: "Signup successful", isSignup: true });
+  } 
+  catch (error) 
+  {
+    console.log("Error", error);
+    res.status(400).json({ message: "Signup failed", isSignup: false });
+  }
+});
+app.get("/", (req, res) => {
+  res.sendFile("C:\\Users\\sanja\\Desktop\\mern\\Day-3\\bcss.html");
+});
 app.listen(port, () => console.log(`Server is running on port: ${port}`));
